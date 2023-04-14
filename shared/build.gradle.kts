df261f13082223523ad6a2e5c068352180607b28
Mehedi Hassan Piash <piash599@gmail.com>
2023-04-14 19:17:28 +0600

    - Update compose multiplatform version 1.4.0

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -45,7 +45,7 @@ kotlin {
             dependencies {
                 api("androidx.activity:activity-compose:1.7.0")
                 api("androidx.appcompat:appcompat:1.6.1")
-                api("androidx.core:core-ktx:1.9.0")
+                api("androidx.core:core-ktx:1.10.0")
                 implementation("io.ktor:ktor-client-okhttp:2.2.4")
             }
         }
