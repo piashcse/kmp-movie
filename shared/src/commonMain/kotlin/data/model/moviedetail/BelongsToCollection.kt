32db9757b72353b94af4daf5c65d61c1f330a829
Mehedi Hassan Piash <piash599@gmail.com>
2023-07-30 23:32:50 +0600

    - Fixing null PointerException for json serialization

--- shared/src/commonMain/kotlin/data/model/moviedetail/BelongsToCollection.kt
+++ shared/src/commonMain/kotlin/data/model/moviedetail/BelongsToCollection.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class BelongsToCollection(
-    val backdrop_path: String,
+    val backdrop_path: String?,
     val id: Int,
     val name: String,
-    val poster_path: String
+    val poster_path: String?
 )
