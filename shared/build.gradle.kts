4a8c684478a11a4f25176099d65df290d9a89d11
Mehedi Hassan Piash <piash599@gmail.com>
2023-08-31 12:05:07 +0600

    - Fixing namespace for common

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -66,7 +66,7 @@ kotlin {
 
 android {
     compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "com.myapplication.common"
+    namespace = "com.kmm.movie.common"
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
