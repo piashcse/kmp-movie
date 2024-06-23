b508d861ae4bb4d000bd140eee1d661382538f35
piashcse <piash599@gmail.com>
2024-06-24 00:15:16 +0600

    - Update Precompose version 1.6.1 - Make kotlin compatible with kotlin

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,16 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import moe.tlaster.precompose.ProvidePreComposeLocals
 
+@OptIn(ExperimentalCoroutinesApi::class)
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "kmp-movie",
     ) {
-        App()
+        ProvidePreComposeLocals {
+            App()
+        }
     }
 }
