573b09b19fd68f685a8660dd8d6ead8389798d1b
Mehedi Hassan Piash <piash599@gmail.com>
2025-01-06 00:12:44 +0600

    - Remove unused imports

--- composeApp/src/commonMain/kotlin/ui/movie/detail/MovieDetail.kt
+++ composeApp/src/commonMain/kotlin/ui/movie/detail/MovieDetail.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.BlurEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
