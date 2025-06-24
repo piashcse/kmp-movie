fb877d0cc8520c595f01bdf0433a311ea7cb48da
Mehedi Hassan Piash <piash599@gmail.com>
2025-06-24 22:22:00 +0600

    - Update kotlin 2.1.21 to 2.2.0 - Update update buildConfig 5.6.6 to 5.6.7

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -109,8 +109,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
